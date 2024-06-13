d0180448814f403effdbe0fc288ff02763f19c71
Marat Shaibakov <smaspb17@yandex.ru>
2024-06-13 20:59:01 +0300

    Add Post model and migrate in blog app

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import TextChoices
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    class Status(TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+
+
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
