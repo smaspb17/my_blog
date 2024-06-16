de609a67a84ec9d127436273c4159754cc5d4cc7
Marat Shaibakov <smaspb17@yandex.ru>
2024-06-16 14:28:35 +0300

    Update admin in blog app

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -3,38 +3,30 @@ from django.db import models
 from django.db.models import TextChoices
 from django.utils import timezone
 
+
 class Post(models.Model):
     class Status(TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT
+        max_length=2, choices=Status.choices, default=Status.DRAFT
     )
 
-
-
-
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
         return self.title
-
-
